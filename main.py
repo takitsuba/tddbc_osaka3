3847f3c5ad5082686422fdabff032ef689d66cdb
takizawa <kei.cataract@gmail.com>
2022-05-14 11:06:12 +0900

    Validate money

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+valid_moneys = (10, 50, 100, 500, 1000)
+
 class VendingMachine:
     total = 0
 
     def refund(self):
         print(f"refund:{self.total}")
         self.total = 0
+
+    def is_valid(self, inserted):
+        return inserted in valid_moneys
 
     def insert_or_refund(self):
         input_str = input("お金を入れてね、払い戻ししたい時はrefundと入力してね:")
@@ -12,7 +17,10 @@ class VendingMachine:
             return
 
         inserted = int(input_str)
-        self.total += inserted
+        if self.is_valid(inserted):
+            self.total += inserted
+        else:
+            print(f"invalid money:{inserted}")
         print(f"total: {self.total}")
 
 
