c74947b1e76e8d28d7f689072e7ed31ff7d0a8a0
takizawa <kei.cataract@gmail.com>
2022-05-14 13:06:01 +0900

    Create setup function to TestVendingMachine

--- test.py
+++ test.py
@@ -3,12 +3,15 @@ import unittest
 from main import VendingMachine
 
 class TestVendingMachine(unittest.TestCase):
-    def test_refund(self):
-        vm = VendingMachine()
-        vm.add_amount(100)
+    def setUp(self) -> None:
+        self.vm = VendingMachine()
 
-        vm.refund()
-        self.assertEqual(vm.amount, 0)
+    def test_refund(self):
+        self.vm.add_amount(100)
+
+        self.vm.refund()
+        self.assertEqual(self.vm.amount, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
